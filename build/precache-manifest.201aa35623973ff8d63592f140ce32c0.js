87316a9772ca51437f37611f8ba1bcae908e8fc9
AdityaNirbhay <94741514+AdityaNirbhay@users.noreply.github.com>
2023-07-05 22:44:16 +0530

    Add files via upload

---
+++ build/precache-manifest.201aa35623973ff8d63592f140ce32c0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "740b9ab283658c75395bf9c6edca02e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a69be9c15b883d50f48",
+    "url": "/static/css/main.0e3533e7.chunk.css"
+  },
+  {
+    "revision": "001b67d05a335e326bff",
+    "url": "/static/js/2.6a7fe4e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a7fe4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a69be9c15b883d50f48",
+    "url": "/static/js/main.41be5fb5.chunk.js"
+  },
+  {
+    "revision": "30259e7a30d4e8b4ab16",
+    "url": "/static/js/runtime-main.a9559881.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "ee198a777bf4537ac495509b9035f099",
+    "url": "/static/media/background.ee198a77.jpg"
+  },
+  {
+    "revision": "2ef9439323f19f30b5734bb8d4f21c58",
+    "url": "/static/media/city.2ef94393.jpg"
+  }
+]);
